76f3726af6b7d79571708bf82c049459a9d44a0b
kyle <kyleiws@uw.edu>
2017-05-22 22:45:33 -0700

    Basic map functionality with center on your location working.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log("Plz work");
+
+var map, infoWindow;
+function initMap() {
+    console.log("callback works");
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 12
+    });
+
+    infoWindow = new google.maps.InfoWindow;
+
+    // Get location from Geolocation object
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Locaiton Found");
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            console.log("ERROR: Geolocaiton found, but not able to load map");
+            infoWindow.open(map);
+        }); 
+    } else {
+        console.log("ERROR: Geolocation not enabled");
+        infoWindow.open(map);
+    };
+}
